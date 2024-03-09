90bde0d107aed853bd5d2d9297e201c416cdb9e6
marirs-dev <marirs.dev@gmail.com>
2024-03-09 12:22:34 +0530

    first commit

---
+++ components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="p-4 bg-white rounded-lg shadow-xl place-content-center text-slate-800"
+  >
+    <p class="text-lg font-bold mb-2">{{ card.Title }}</p>
+    <p class="text-sm">{{ card.Content }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ card: { Title: string; Content: string } }>();
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-3xl text-center pt-8">Data from Google Sheets</h1>
+  <div v-for="(card, index) in sheetResonse?.data" :key="index" class="p-4">
+    <Card :card="card" />
+  </div>
+</template>
+<script setup lang="ts">
+const { data: sheetResonse } = await useFetch<{
+  data: { data: { Title: string; Content: string } };
+}>(
+  "https://script.google.com/macros/s/AKfycbw1BSuTvrLyIMHykdV8uH1WCzyCvEJMsQZgSSs0oi51ys863jn1u8ZEhF0IuGwJIsS0HA/exec",
+  {
+    headers: {}
+  }
+);
+</script>
